be09bdcd3176989a6e052898767eb7be23a4d955
ackzf <armageddonkfrost@gmail.com>
2013-02-15 16:20:18 -0800

    Update login/management/commands/serverTest.py

--- login/management/commands/serverTest.py
+++ login/management/commands/serverTest.py
@@ -22,12 +22,14 @@ class TestUsers(unittest.TestCase):
         """
         Tests that adding a user works
         """
+        self.users.reset()
         self.assertEquals(server.SUCCESS, self.users.add("user1", "password"))
 
     def testAddExists(self):
         """
         Tests that adding a duplicate user name fails
         """
+        self.users.reset()
         self.assertEquals(server.SUCCESS, self.users.add("user1", "password"))
         self.assertEquals(server.ERR_USER_EXISTS, self.users.add("user1", "password"))
 
@@ -35,6 +37,7 @@ class TestUsers(unittest.TestCase):
         """
         Tests that adding two users works
         """
+        self.users.reset()
         self.assertEquals(server.SUCCESS, self.users.add("user1", "password"))
         self.assertEquals(server.SUCCESS, self.users.add("user2", "password"))
 
@@ -42,8 +45,48 @@ class TestUsers(unittest.TestCase):
         """
         Tests that adding an user with empty username fails
         """
+        self.users.reset()
         self.assertEquals(server.ERR_BAD_USERNAME, self.users.add("", "password"))
-
+        
+    def testAddLogin(self):
+        self.users.reset()
+        self.assertEquals(server.SUCCESS, self.users.add("user1", "password"))
+        self.assertEquals(2, self.users.login(self, "user1", "password"))
+        
+    def testAddLongUser(self):
+        self.users.reset()
+        a = ""
+        for i in range(0,127)
+            a += "a"
+        self.assertEquals(server.SUCCESS, self.users.add(a, "password"))
+        aa = a + "aa"
+        self.assertEquals(server.ERR_BAD_USERNAME, self.users.add(aa, "password"))
+    
+    def testAddLongPword(self):
+        self.users.reset()
+        a = ""
+        for i in range(0,127)
+            a += "a"
+        self.assertEquals(server.SUCCESS, self.users.add("user1", a))
+        aa = a + "aa"
+        self.assertEquals(server.ERR_BAD_PASSWORD, self.users.add("user2", aa))
+    
+    def testLongIser:
+        self.users.reset()
+        a = ""
+        for i in range(0,127)
+            a += "a"
+        self.assertEquals(server.SUCCESS, self.users.add(a, "pass"))
+        self.assertEquals(2, self.users.login(self, a, "pass"))
+    
+    def testLongPword:
+        self.users.reset()
+        a = ""
+        for i in range(0,127)
+            a += "a"
+        self.assertEquals(server.SUCCESS, self.users.add("user1", a))
+        self.assertEquals(2, self.users.login(self, "user1", a))
+        
 
 # If this file is invoked as a Python script, run the tests in this module
 if __name__ == "__main__":
